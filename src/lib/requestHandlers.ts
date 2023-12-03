69f52170863fbc9ecb4d75c8a87a7fb806070244
Klaus Dieter Kupper <klausdk1999@gmail.com>
2023-12-03 00:14:13 -0300

    fix: url

--- src/lib/requestHandlers.ts
+++ src/lib/requestHandlers.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://go-pe.onrender.com/",
 });
 
 export const getUsers = async () => {
